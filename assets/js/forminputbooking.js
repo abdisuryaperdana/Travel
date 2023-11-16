2d7214d8ecafcbc00d6e8da481dd7545e63d0b76
Abdi Surya Perdana <abdisury4@gmail.com>
2023-11-16 15:58:49 +0700

    add logic fe add data with post api

--- assets/js/forminputbooking.js
+++ assets/js/forminputbooking.js
@@ -15,6 +15,7 @@ function openPopupForm(i) {
         var jumlahOrang = document.getElementById('jumlah-orang').value;
         var hari = document.getElementById('hari-tgl').value;
         var kegiatan = document.getElementById('kegiatan').value;
+        var jam = document.getElementById('jam').value;
   
       
         // Lakukan sesuatu dengan nilai inputan, misalnya kirim ke server atau tampilkan di console
@@ -23,6 +24,7 @@ function openPopupForm(i) {
         console.log('Jumlah Orang:', jumlahOrang);
         console.log('Hari:', hari);
         console.log('Kegiatan:', kegiatan);
+        console.log('jam',jam);
       
         // Tutup pop-up form
         closePopupForm();
@@ -33,6 +35,8 @@ function openPopupForm(i) {
         var jdwl_festival = document.getElementById('jadwal-festival').value;
         var lokasi_festival = document.getElementById('lokasi-festival').value;
         var gmaps_festival = document.getElementById('gmaps-festival').value;
+        var foto_festival = document.getElementById('foto-festival').value;
+        var jenis = 'Festival';
   
       
         // Lakukan sesuatu dengan nilai inputan, misalnya kirim ke server atau tampilkan di console
@@ -41,6 +45,8 @@ function openPopupForm(i) {
         console.log('jdwl_festival:', jdwl_festival);
         console.log('lokasi_festival:', lokasi_festival);
         console.log('gmaps_festival:', gmaps_festival);
+        console.log('foto_festival:', foto_festival);
+        console.log('Festival', jenis);
       
         // Tutup pop-up form
         closePopupForm();
@@ -51,6 +57,8 @@ function openPopupForm(i) {
         var jdwl_pameran = document.getElementById('jadwal-pameran').value;
         var lokasi_pameran = document.getElementById('lokasi-pameran').value;
         var gmaps_pameran = document.getElementById('gmaps-pameran').value;
+        var foto_pameran = document.getElementById('foto-pameran').value;
+        var jenis = 'Pameran';
   
       
         // Lakukan sesuatu dengan nilai inputan, misalnya kirim ke server atau tampilkan di console
@@ -59,6 +67,8 @@ function openPopupForm(i) {
         console.log('jdwl_pameran:', jdwl_pameran);
         console.log('lokasi_pameran:', lokasi_pameran);
         console.log('gmaps_pameran:', gmaps_pameran);
+        console.log('foto_pameran:', foto_pameran);
+        console.log('Pameran', jenis);
       
         // Tutup pop-up form
         closePopupForm();
